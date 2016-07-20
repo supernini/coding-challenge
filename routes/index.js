e2c082e632dd36fb0961f1d3f459c22ca3ed2cdd
Denis Fabien <denis.fabien.ca@gmail.com>
2016-07-19 23:40:10 -0400

    Removing duplicate code.

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs')
 
-var bodyParser = require('body-parser')
-router.use( bodyParser.json() );
-router.use(bodyParser.urlencoded({
-  extended: true
-}));
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Osedea' });
